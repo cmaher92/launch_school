c4873a277e0f4c6cbeedc52ee7ee044c51e0799f
Connor Maher <connormmaher@gmail.com>
2017-02-27 19:04:47 -0500

    complete roman_to_int.rb exercize

--- back_end_prep/learn_to_program/roman_to_integer.rb
+++ back_end_prep/learn_to_program/roman_to_integer.rb
@@ -1,31 +1,108 @@
 # roman to int takes roman nums and turms them into ints
 
-def roman_to_int number
+require 'pry'
+
+def roman_to_int roman_numerals
+  roman_numerals = roman_numerals.downcase
+  number = 0
+  is_roman = is_roman? roman_numerals
+  if is_roman == false
+    puts 'Please enter a valid roman numeral'
+    return
+  end
+
+  number = 0
+  prev_numeral = ''
+
+  # check if previous numeral was less than current numeral
+  roman_numerals.each_char do |numeral|
+    # binding.pry
+    if numeral == 'm'
+      if prev_numeral == 'c'
+        number += 800
+        prev_numeral = numeral
+      else
+        number += 1000
+        prev_numeral = numeral
+      end
+    elsif numeral == 'd'
+      if prev_numeral == 'c'
+        number += 300
+        prev_numeral = numeral
+      else
+      number += 500
+      prev_numeral = numeral
+      end
+    elsif numeral == 'c'
+      if prev_numeral == 'x'
+        number += 80
+        prev_numeral = numeral
+      else
+        number += 100
+        prev_numeral = numeral
+      end
+    elsif numeral == 'l'
+      if prev_numeral == 'x'
+        number += 30
+        prev_numeral = numeral
+      else
+        number += 50
+        prev_numeral = numeral
+      end
+    elsif numeral == 'x'
+      if prev_numeral == 'i'
+        number += 8
+        prev_numeral = numeral
+      else
+        number += 10
+        prev_numeral = numeral
+      end
+    elsif numeral == 'v'
+      if prev_numeral == 'i'
+        number += 3
+        prev_numeral = numeral
+      else
+        number += 1
+        prev_numeral = numeral
+      end
+    elsif numeral == 'i'
+      number += 1
+      prev_numeral = numeral
+    end
+  end
+  return number
 end
 
 def is_roman? roman_string
-  is_numeral = true
-  while is_numeral == true
-    roman_string.each do |numeral|
-     if numeral == 'i'
-       return
+    valid_numeral = true
+    roman_string.each_char do |numeral|
+      # check if numeral is a proper roman numeral
+      # if not return false
+      if numeral == 'm'
+        next
+      elsif numeral == 'i'
+        next
       elsif numeral == 'v'
-        return
+        next
       elsif numeral == 'x'
-        return
+        next
       elsif numeral == 'l'
-        return
+        next
       elsif numeral == 'c'
-        return
+        next
       elsif numeral == 'd'
-        return
-      elsif numeral == 'm'
-        return
+        next
       else
-        is_numeral = false
-        puts "please enter a valid roman numeral."
+        valid_numeral = false
       end
     end
-   end
+    return valid_numeral
+end
 
-puts is_roman? 'ivxlcdmn'
+puts roman_to_int 'cm'
+puts roman_to_int 'l'
+puts roman_to_int 'd'
+puts roman_to_int 'xl'
+puts roman_to_int 'xliii'
+puts roman_to_int 'cmxliv'
+puts roman_to_int 'mcmxcix'
