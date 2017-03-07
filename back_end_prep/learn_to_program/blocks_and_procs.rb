7c89a053ddd8ba3b8ac5b645b6f3e14a3c78a643
Connor Maher <connormmaher@gmail.com>
2017-03-06 23:59:25 -0500

    finished profiling practice

--- back_end_prep/learn_to_program/blocks_and_procs.rb
+++ back_end_prep/learn_to_program/blocks_and_procs.rb
@@ -96,3 +96,28 @@ class Array
   [1, 2, 3, 4, 5].each_even do |odd_ball|
     puts "#{odd_ball} is NOT an even number!"
   end
+end
+
+# Another example of creating a method that takes a block
+def profile block_description, &block
+  start_time = Time.new
+  block.call
+  duration = Time.new - start_time
+  puts "#{block_description}: #{duration} seconds"
+end
+
+profile '2000 dublings' do
+  number = 1
+  2500.times do
+    number = number + number
+  end
+  puts "#{number.to_s.length} digits"
+  # that's the number of digits in the number
+end
+
+profile 'count to a million' do
+  number = 0
+  1000000.times do
+    number = number + 1
+  end
+end
