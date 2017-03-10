c651f3db51d7320fbb20b5e36b30c7517526d69e
Connor Maher <connormmaher@gmail.com>
2017-03-10 05:35:54 +0000

    completed loops and iterators exercises

---
+++ back_end_prep/intro_to_programming_with_ruby/arrays/exercise_2.rb
@@ -0,0 +1,9 @@
+# Exercise 2
+# Ruby Arrays
+
+arr = ["b", "a"]
+arr = arr.product(Array(1..3))
+# [["b", "1"], ["b", "2"], ["b", "3"], ["a", "1"], ["a", "2"], ["a", "3"]]
+arr.first.delete(arr.first.last)
+
+# => 1
