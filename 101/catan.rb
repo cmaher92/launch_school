f06c37b49e091547ac1aacae75ff39abccaab6be
Connor Maher <connormmaher@gmail.com>
2017-03-30 01:53:05 -0400

    Worked on the randomize function and the creating the map function

--- 101/catan.rb
+++ 101/catan.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # Catan
 # Randomize a catan map
 # Calculate the best position to place a settlement
@@ -22,27 +24,79 @@
 # Brick - 3
 # Desert - 1
 
-W = 'wheat'
-F = 'forest'
-S = 'sheep'
-O = 'Ore'
-B = 'Brick'
-D = 'Desert'
+# W = 'wheat'
+# F = 'forest'
+# S = 'sheep'
+# O = 'Ore'
+# B = 'Brick'
+# D = 'Desert'
 
 # Generate an array of 19 resources and randomize them
 resource_options = { W: 4, F: 4, S: 4, O: 3, B: 3, D: 1 }
 
-def randomize(resource_options)
-  total_count = 0
+def randomize_order(resource_options)
+  # Creates an array with 19 elements randomized
+  total_count = 19
 
-  # While total_count > 0
-  #   select random_resource from resource_options
-  #   if resource_options[random_resource] > 0
-  #     add it to the resources array
-  #     subtract 1 from that resource
-  #     also subtract 1 crom total_count
-  #   elsif
-  #     next
+  resources = []
 
-  while
+  while total_count > 0
+    resource_options_arr = resource_options.to_a
+    rand_index = rand(resource_options_arr.length)
+    rand_resource = resource_options_arr[rand_index].first
+
+    if resource_options[rand_resource] > 0
+        resources.push rand_resource
+        resource_options[rand_resource] -= 1
+        total_count -= 1
+    end
+  end
+
+  resources
 end
+
+def create_map(rand_list_of_resources)
+    map = [
+    [],
+    [],
+    [],
+    [],
+    [],
+  ]
+
+  height = 0
+
+  while height <= 4
+    if height == 0
+      3.times { map[0] << rand_list_of_resources.pop }
+      height += 1
+    elsif height == 1
+      4.times { map[1] << rand_list_of_resources.pop }
+      height += 1
+    elsif height == 2
+      5.times { map[2] << rand_list_of_resources.pop }
+      height += 1
+    elsif height == 3
+      4.times { map[3] << rand_list_of_resources.pop }
+      height += 1
+    elsif height == 4
+      3.times { map[4] << rand_list_of_resources.pop }
+      height += 1
+    end
+  end
+  map
+end
+
+# Randomizes the resources
+p rand_list_of_resources = randomize_order(resource_options)
+# Builds the map
+p create_map(rand_list_of_resources)
+
+# function that assigns roll numbers to each resource, takes an array, returns a hash
+# turn array into hash
+# create a list of all the rolls, rolls = [ 2, 3, 3, 4, 4,  ]
+# TODO: will need to adjust the create_map function to handle a hash instead of an array
+# randomize the list of rolls
+#
+# iterate through the hash of
+# assign each roll to a resource unless the resource is a Desert, then skip
