3db781dc54a7c22d897b27cee243a625caa3670f
Connor Maher <connormmaher@gmail.com>
2017-04-08 04:19:16 +0000

    working on RPS bonus features

--- 101/lesson_2/rock_paper_scissors.rb
+++ 101/lesson_2/rock_paper_scissors.rb
@@ -1,5 +1,5 @@
 
-VALID_CHOICES = %w( rock paper scissors )
+VALID_CHOICES = %w(rock paper scissors lizard spock).freeze
 
 def test_method
   prompt('test method')
@@ -10,18 +10,20 @@ def prompt(message)
   # interpolated
 end
 
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
 def display_results(player, computer)
   # Note the usage of 'display' clearly describing that this method will print
-  if (player == 'rock' && computer == 'scissors') ||
-     (player == 'paper' && computer == 'rock' ) ||
-     (player == 'scissors' && computer == 'paper' )
-  prompt('You won!')
-  elsif (player == 'rock' && computer == 'paper') ||
-        (player == 'paper' && computer == 'scissors' ) ||
-        (player == 'scissors' && computer == 'rock' )
+  if win?(player, computer)
+    prompt('You won!')
+  elsif win?(computer, player)
     prompt('Computer won!')
   else
-  prompt("It's a tie!")
+    prompt("It's a tie!")
   end
 end
 
@@ -32,16 +34,16 @@ loop do
     choice = gets.chomp
     # case sensitive, need to handle that
     break if VALID_CHOICES.include?(choice)
-    prompt("Invalid choice, please try again.")
+    prompt('Invalid choice, please try again.')
   end
-  
+
   computer_choice = VALID_CHOICES.sample
-  
+
   prompt("You chose #{choice} and the computer chose #{computer_choice}")
-  
+
   display_results(choice, computer_choice)
-  
+
   prompt('Do you want to play again? (y/n)')
   play_again = gets.chomp.downcase
-  break if play_again == 'n' || 'no'
+  break if play_again == 'n' || play_again == 'no'
 end
