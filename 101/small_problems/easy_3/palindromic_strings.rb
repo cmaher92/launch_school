2a8ea31ab30ae1f85fbf8d4b1d6d224238d27e68
Connor <connormmaher@gmail.com>
2017-06-15 14:32:42 -0400

    completed easy_3 problem set

--- 101/small_problems/easy_3/palindromic_strings.rb
+++ 101/small_problems/easy_3/palindromic_strings.rb
@@ -14,31 +14,50 @@
 # given the input string, take the string and reverse it
 # if the string is equal, return true
 # otherwise, return false
+class Palindrome
+  def palindrome?(str)
+    str == str.reverse
+  end
 
-def palindrome?(str)
-  str == str.reverse
+  # test cases
+  puts "pass" if palindrome?('madam') == true
+  puts "pass" if palindrome?('Madam') == false          # (case matters)
+  puts "pass" if palindrome?("madam i'm adam") == false # (all characters matter)
+  puts "pass" if palindrome?('356653') == true
+
+
+  # understanding
+  # input
+  #   array
+  # output
+  #   boolean, true if the array is palandromic false otherwise
+
+  # algorithm
+  #   reverse the array given and check if its equal to original array
+  # return boolean
+
+  def palindrome_array?(arr)
+    arr == arr.reverse
+  end
+
+  puts "pass" if palindrome_array?([1, 2, 1]) == true
+  puts "pass" if palindrome_array?(["foo", "bar", "baz"]) == false
+
+  # understanding
+  # input
+  #   string or array
+  # output
+  #   boolean whether or not the string/array passed in is palindromic
+
+  # algorithm
+  #   .reverse works on both str and array
+  #   item == item.reverse
+
+  def palindromic?(item)
+    item == item.reverse
+  end
+
+  puts "pass" if palindromic?("sam mas") == true
+  puts "pass" if palindromic?(['foo', 'bar', 'foo']) == true
+  puts "pass" if palindromic?('foo bar baz') == false
 end
-
-# test cases
-puts "pass" if palindrome?('madam') == true
-puts "pass" if palindrome?('Madam') == false          # (case matters)
-puts "pass" if palindrome?("madam i'm adam") == false # (all characters matter)
-puts "pass" if palindrome?('356653') == true
-
-
-# understanding
-# input
-#   array
-# output
-#   boolean, true if the array is palandromic false otherwise
-
-# algorithm
-#   reverse the array given and check if its equal to original array
-# return boolean
-
-def palindrome_array?(arr)
-  arr == arr.reverse
-end
-
-puts "pass" if palindrome_array?([1, 2, 1]) == true
-puts "pass" if palindrome_array?(["foo", "bar", "baz"]) == false
