c73e71fb3eb323f75fe8f053cc851ab1fcdaeeee
Connor Maher <connormmaher@gmail.com>
2017-06-21 03:25:17 +0000

    Added Launch School's solution to num_to_string problem

--- 101/small_problems/easy_4/num_to_str.rb
+++ 101/small_problems/easy_4/num_to_str.rb
@@ -25,22 +25,37 @@
 # map the integers to their string representations
 # join the array and return
 
-def integer_to_string(integer)
-    int_to_str = { 
-        1 => '1',
-        2 => '2',
-        3 => '3',
-        4 => '4',
-        5 => '5',
-        6 => '6',
-        7 => '7',
-        8 => '8',
-        9 => '9',
-        0 => '0'
-    }
-    integer = integer.digits
-    integer.map! { |digit| int_to_str[digit] }
-    integer.join.reverse
+# def integer_to_string(integer)
+#     int_to_str = { 
+#         1 => '1',
+#         2 => '2',
+#         3 => '3',
+#         4 => '4',
+#         5 => '5',
+#         6 => '6',
+#         7 => '7',
+#         8 => '8',
+#         9 => '9',
+#         0 => '0'
+#     }
+#     integer = integer.digits
+#     integer.map! { |digit| int_to_str[digit] }
+#     integer.join.reverse
+# end
+
+# result provided
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+def integer_to_string(number)
+    result = ''
+    
+    loop do
+        number, remainder = number.divmod(10)
+        result.prepend(DIGITS[remainder])
+        
+        break if number == 0
+    end
+    result
 end
 
 # test cases
