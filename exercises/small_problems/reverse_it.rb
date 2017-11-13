b8522ab9f51fc036f497027d816e02c6fd26f3d5
Connor Maher <connormmaher@gmail.com>
2017-11-12 22:37:26 -0500

    added easy_1 problems

---
+++ exercises/small_problems/reverse_it.rb
@@ -0,0 +1,10 @@
+Reverse It (Part 1)
+
+Write a method that takes one argument, a string, and returns the same string with the words in reverse order.
+
+Examples:
+
+puts reverse_sentence('') == ''
+puts reverse_sentence('Hello World') == 'World Hello'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
+The tests above should print true.
