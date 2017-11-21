7e2c3d082748d89dc547849c44e48db7b2d5ad09
Connor Maher <connormmaher@gmail.com>
2017-11-21 19:38:02 +0000

    read the mandatory_block.rb problem

--- 101/exercises/ruby_basics/documentation_2/mandatory_blocks.rb
+++ 101/exercises/ruby_basics/documentation_2/mandatory_blocks.rb
@@ -1,22 +1,37 @@
-Mandatory Blocks
+# Mandatory Blocks
 
-The Array#bsearch method is used to search ordered Arrays more quickly than #find and #select can. Assume you have the following code:
+# The Array#bsearch method is used to search ordered Arrays more quickly than #find and #select can. Assume you have the following code:
 
 a = [1, 4, 8, 11, 15, 19]
-How would you search this Array to find the first element whose value exceeds 8?
+# How would you search this Array to find the first element whose value exceeds 8?
 
-Hide Solution & Discussion
-Solution
+a.each do |element|
+  if element > 8
+    puts element
+    break
+  end
+end
+
+# Solution
 
 value = a.bsearch { |element| element > 8 }
 puts value # => 11
-Discussion
 
-The documentation for Array#bsearch has the signature:
+# The documentation for Array#bsearch has the signature:
 
-bsearch {|x| block } → elem
-This signature shows that #bsearch takes only one argument - a block - and returns an element of the Array to which it is applied. We can also see that the block is required, and that it takes one argument. The text and examples below the signature tell and show us that the block must return true or false when operating in what is called "find-minimum mode", which is the usual use case.
+# bsearch {|x| block } → elem
+# This signature shows that #bsearch takes only one argument - a block - 
+# and returns an element of the Array to which it is applied. 
+# We can also see that the block is required, and that it takes one argument. 
+# The text and examples below the signature tell and show us that the block must 
+# return true or false when operating in what is called "find-minimum mode", 
+# which is the usual use case.
 
-Putting this all together, we can devise our call to #bsearch: we use a as the receiver (the object to which #bsearch is applied), a block of { |element| element > 8 } to find the first element whose value is greater than 8, and we store the return value to a variable named value. The value of value is then printed, which shows that #bsearch found the 11 in a.
+# Putting this all together, we can devise our call to #bsearch: we use a 
+# as the receiver (the object to which #bsearch is applied), a block of 
+# { |element| element > 8 } to find the first element whose value is greater 
+# than 8, and we store the return value to a variable named value. The value of 
+# value is then printed, which shows that #bsearch found the 11 in a.
 
-For more information on what #bsearch does and why it is faster, see the Wikipedia article on the Binary search algorithm. #bsearch uses this algorithm.
+# For more information on what #bsearch does and why it is faster, see the 
+# Wikipedia article on the Binary search algorithm. #bsearch uses this algorithm.
