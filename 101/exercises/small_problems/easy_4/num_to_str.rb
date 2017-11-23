ff77ee67713df30e3d48f153d3875dcf45028d35
Connor Maher <connormmaher@gmail.com>
2017-11-23 16:34:57 -0500

    working on num_to_str problem

---
+++ 101/exercises/small_problems/easy_4/num_to_str.rb
@@ -0,0 +1,57 @@
+# Convert a Number to a String!
+#
+# In the previous two exercises, you developed methods that convert simple
+# numeric strings to signed Integers. In this exercise and the next, you're
+# going to reverse those methods.
+#
+# Write a method that takes a positive integer or zero,
+# and converts it to a string representation.
+#
+# You may not use any of the standard conversion methods available in Ruby,
+# such as Integer#to_s, String(), Kernel#format, etc.
+# Your method should do this the old-fashioned way and construct the string
+# by analyzing and manipulating the number.
+
+# input
+#   integer - positive or 0
+# output
+#   string representation of the inputted int
+# rules
+#   no stand conversion methods
+#
+# given an integer that is 0 or positive
+# break the integer up into an array
+#   divide by x
+#     x is determined by the size of the number
+#     if the number is between 10 and 99 / 10
+#     if the number is between 100 and 999 / 100
+#     if the number is between 1000 and 9999 / 1000
+#     if the number is between 10000 and 99999 / 10000
+#   if less than 10 modulo by 10
+# map each integer to it's string representation
+# combine the array into a string
+# return the string
+
+def integer_to_string(int)
+  arr_of_ints = []
+  while true
+    if int < 10
+      int = int % 10
+      arr_of_ints << int
+      break
+    end
+    if int >= 10 && int < 100
+      arr_of_ints << int / 10
+      int = int % 10
+      next
+    end
+  end
+  arr_of_ints.inspect
+end
+
+puts integer_to_string(97)
+
+# Examples
+# integer_to_string(4321) == '4321'
+# integer_to_string(0) == '0'
+# integer_to_string(5000) == '5000'
