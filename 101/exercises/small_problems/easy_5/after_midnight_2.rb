a5e4bf0fa036079f00394a592b8ce9f624657def
Connor Maher <connormmaher@gmail.com>
2017-11-26 19:23:20 -0500

    finished after_ midnight 2 further exploration

--- 101/exercises/small_problems/easy_5/after_midnight_2.rb
+++ 101/exercises/small_problems/easy_5/after_midnight_2.rb
@@ -18,28 +18,45 @@
 # calculate the total minutes and return
 #   multiply each hour by 60 and add to the minutes
 
+require 'date'
+
 MINUTES_PER_DAY = 1440
+MIDNIGHT = '00:00'
 
 def after_midnight(time)
-  hours_minutes = parse_time(time)
-  hours_minutes[0] = 0 if hours_minutes[0] == 24
-  (hours_minutes[0] * 60) + hours_minutes[1]
+  if time == '24:00'
+    time = MIDNIGHT
+  end
+  date = DateTime.parse(MIDNIGHT)
+  date = date.to_time
+  new_date = DateTime.parse(time)
+  new_date = new_date.to_time
+  (new_date - date) / 60
 end
 
 def before_midnight(time)
-  hours_minutes = parse_time(time)
-  hours_minutes[0] = 0 if hours_minutes[0] == 24
-  total_minutes = (hours_minutes[0] * 60) + hours_minutes[1]
-  if total_minutes == 0
-    return 0
-  end
-  MINUTES_PER_DAY % total_minutes
+  return 0 if time == '00:00' || time == '24:00'
+
+  date = DateTime.parse(MIDNIGHT)
+  date = date.to_time
+
+  new_date = DateTime.parse(time)
+  # puts the date a day prior
+  new_date = new_date - 1
+  new_date = new_date.to_time
+
+  (date - new_date) / 60
 end
 
 def parse_time(time)
   time = time.split(':')
   time.map! { |element| element.to_i }
 end
+
+# How would these change if you were allowed to use Date and Time classes?
+# create a date for midnight
+# create a second date for the given time
+
 
 #
 # Test cases
