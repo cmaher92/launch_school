6f7b9cd12235541a42abc0f054d72702fe15f6b4
Connor Maher <connormmaher@gmail.com>
2017-11-27 14:37:07 +0000

    completed letter_counter problem

--- 101/exercises/small_problems/easy_5/letter_counter.rb
+++ 101/exercises/small_problems/easy_5/letter_counter.rb
@@ -1,12 +1,40 @@
-Letter Counter (Part 1)
+# Letter Counter (Part 1)
 
-Write a method that takes a string with one or more space separated words and returns a hash that shows the number of words of different sizes.
+# Write a method that takes a string with one or more space separated words 
+# and returns a hash that shows the number of words of different sizes.
 
-Words consist of any string of characters that do not include a space.
+# Words consist of any string of characters that do not include a space.
 
-Examples
+# input
+#   string, a sentence with a single spaced out words
+# output 
+#   hash, the key represents number of chars in a word and the value is number of
+#   words found with that many chars
 
-word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
-word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
-word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
-word_sizes('') == {}
+# given a sentence
+# create an empty hash for word_size
+# split the words in an array of words
+# for each word in the array 
+#   count the chars and check if there is a key match for that size, 
+#   if there is increment the value
+#   otherwise add it to the hash
+# return the hash
+
+def word_sizes(sentence)
+  word_size = {}
+  arr = sentence.split(' ')
+  arr.each do |word|
+    if word_size.has_key?(word.size)
+      word_size[word.size] += 1
+    else 
+      word_size[word.size] = 1
+    end
+  end
+  word_size
+end
+
+# Test Cases
+puts word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
+puts word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+puts word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+puts word_sizes('') == {}
