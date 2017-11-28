5212031322d0f1805e3105bb67ef605a59079a39
Connor Maher <connormmaher@gmail.com>
2017-11-28 22:34:23 +0000

    worked on some problems

--- 101/exercises/small_problems/easy_5/alphabetical_nums.rb
+++ 101/exercises/small_problems/easy_5/alphabetical_nums.rb
@@ -10,40 +10,15 @@
 #   Array of integer between 0 and 19
 # output
 #   Integer, sorted based on the english word for each number
+NUMBER_WORDS = %w(zero one two three four
+                  five six seven eight nine
+                  ten eleven twelve thirteen fourteen
+                  fifteen sixteen seventeen eighteen nineteen)
 
-
-def alphabetic_number_sort(arr)
-  num_converter = {
-0 => 'zero',
-  1 => 'one',
-  2 => 'two',
-  3 => 'three',
-  4 => 'four', 
-  5 => 'five',
-  6 => 'six',
-  7 => 'seven',
-  8 => 'eight',
-  9 => 'nine',
-  10 => 'ten',
-  11 => 'eleven',
-  12 => 'twelve',
-  13 => 'thirteen',
-  14 => 'fourteen',
-  15 => 'fifteen',
-  16 => 'sixteen',
-  17 => 'seventeen',
-  18 => 'eighteen',
-  19 => 'nineteen'
-}
-  # sort the array
-  # map each number in the array
-  arr.sort.map! { |num| num_converter[num] }
-  # flip the hash
-  num_converter = num_converter.each_pair { |key, value| key, value = value, key }
-  puts num_converter.inspect
-  # map the harr again
-  # return arr
+def alphabetic_number_sort(numbers)
+  numbers.sort_by { |number| NUMBER_WORDS[number] }
 end
+
 
 # Test Cases
 puts alphabetic_number_sort((0..19).to_a) == [
