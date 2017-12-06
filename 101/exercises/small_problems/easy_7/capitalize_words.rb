1e770ad666d3ca9d2176fd1388cc25fc12b76c60
Connor Maher <connormmaher@gmail.com>
2017-12-06 22:39:04 +0000

    finish 2 easy_2 problems and further exploration

--- 101/exercises/small_problems/easy_7/capitalize_words.rb
+++ 101/exercises/small_problems/easy_7/capitalize_words.rb
@@ -1,15 +1,75 @@
+=begin
 Capitalize Words
 
-Write a method that takes a single String argument and returns a new string that contains the original value of the argument with the first character of every word capitalized and all other letters lowercase.
+Write a method that takes a single String argument and returns a new string 
+that contains the original value of the argument with the first character of 
+every word capitalized and all other letters lowercase.
 
 You may assume that words are any sequence of non-blank characters.
+=end
 
-Examples
+# input
+#   string
+# output
+#   string, capitalize all words in string 
 
-word_cap('four score and seven') == 'Four Score And Seven'
-word_cap('the javaScript language') == 'The Javascript Language'
-word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
-Hide Solution & Discussion
+# given the string
+# break it up into an array
+# for each word in the array, capitalize
+# join the string with spaces
+# return
+
+# My solution
+# def word_cap(str)
+#   p str.split.map(&:capitalize).join(' ')
+# end
+
+# Further exploration
+# come up with a solution not using capitalize
+
+# map each word in the arr
+#   access the 1st element in the word
+#   change it to a capital version
+#   change remaining string to lowercase
+
+# def word_cap(str)
+#   arr   = str.split
+#   count = 0
+#   loop do
+#     break if count == arr.length
+#     arr[count][0] = arr[count][0].upcase
+#     arr[count][1..-1] = arr[count][1..-1].downcase
+#     count += 1
+#   end
+#   arr.join(' ')
+# end
+
+# another solution
+# given a string
+# split into an array on the spaces
+# use regex to change each lowercase first character in each word to uppercase
+# use regex to change the remaining characters in the word to lowercase
+require 'pry'
+
+def word_cap(str)
+  arr    = str.split
+  result = []
+  arr.each do |word|
+    word = word.gsub(/(.)/, &:upcase)
+    word = word.gsub(/.{1}(.*)$/, &:downcase)
+    binding.pry
+    result << word
+  end
+  p result.join(' ')
+end
+  
+
+# Test Cases
+p word_cap('four score and seven') == 'Four Score And Seven'
+p word_cap('the javaScript language') == 'The Javascript Language'
+p word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
+
+=begin
 Solution
 
 def word_cap(words)
@@ -23,15 +83,20 @@ We can also write this more succinctly as:
 def word_cap(words)
   words.split.map(&:capitalize).join(' ')
 end
-Discussion
 
-In the second solution, (&:method_name) is shorthand notation for { |item| item.method_name }. Thus, &:capitalize translates to:
+Discussion
+In the second solution, (&:method_name) is shorthand notation for 
+{ |item| item.method_name }. Thus, &:capitalize translates to:
 
 do |word|
   word.capitalize
 end
+
 We can use this shorthand syntax with any method that has no required arguments.
 
 Further Exploration
 
-Ruby convenient provides the String#capitalize method to capitalize strings. Without that method, how would you solve this problem? Try to come up with at least two solutions.
+Ruby convenient provides the String#capitalize method to capitalize strings. 
+Without that method, how would you solve this problem? Try to come up with at 
+least two solutions.
+=end
--- 101/exercises/small_problems/easy_7/lettercase.rb
+++ 101/exercises/small_problems/easy_7/lettercase.rb
@@ -1,13 +1,49 @@
+=begin
 Lettercase Counter
 
-Write a method that takes a string, and then returns a hash that contains 3 entries: one represents the number of characters in the string that are lowercase letters, one the number of characters that are uppercase letters, and one the number of characters that are neither.
+Write a method that takes a string, and then returns a hash that contains 3 
+entries: one represents the number of characters in the string that are 
+lowercase letters, one the number of characters that are uppercase letters, 
+and one the number of characters that are neither.
 
-Examples
+input
+  string
+hash
+  numbers in characters in string lowercase
+  number of characters that are uppercase
+  neither characters that are not upper or lowercase
+  
+  create a hash using new functionality to set the value's default to 0
+  turn the string into an array
+    for each character in the string
+    if it's not a string, incremement :neither
+    if it's uppercase, increment the hash
+    if it's lowercase, incremenet the hash
+  return the hash
+=end
 
-letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
-letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
-letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
-letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
+def letter_case_count(str)
+  chars = {:neither => 0, :uppercase => 0, :lowercase => 0}
+  str.chars.each do |char|
+    case
+      when char =~ (/[^a-z]/i) then chars[:neither]   += 1
+      when char =~ (/[A-Z]/)   then chars[:uppercase] += 1
+      when char =~ (/[a-z]/)   then chars[:lowercase] += 1
+    end
+  end
+  chars
+end 
+
+p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
+p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
+p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
+p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
+
+
+
+
+
+=begin
 Hide Solution & Discussion
 Solution
 
@@ -19,6 +55,7 @@ def letter_case_count(string)
   counts[:neither] = characters.count { |char| char =~ /[^A-Za-z]/ }
   counts
 end
+
 Discussion
 
 This method is expected to return a hash of character counts, so we'll create a counts hash to store that data.
@@ -30,3 +67,4 @@ With that we have the data needed to satisfy this problem's requirements: a hash
 We use regular expressions to count certain types of characters. We'll explain things a bit here for those who aren't familiar with regex:
 
 char =~ /[a-z]/ : This checks if the character is a lowercase letter between a and z. char =~ /[A-Z]/ : This checks if the character is an uppercase letter between A and Z. char =~ /[^A-Za-z]/ : This checks if the character is neither an uppercase letter nor a lowercase letter.
+=end
