02175306a60fd22ecccfd9b4503041c3d16e794f
Connor Maher <connormmaher@gmail.com>
2017-12-12 23:49:29 -0500

    working on ceasar_cipher.rb

---
+++ codewars/ceasar_cipher.rb
@@ -0,0 +1,21 @@
+def movingShift(s, shift)
+=begin
+    input
+      string - s, that needs coding
+      int - shift, the number for the initial shift
+    output
+      arr - string split into fifths such that the first four arrays must try to be
+            equal length, and then the rest can go in the fifth array as long as it's
+            not longer than the first
+=end
+end
+
+def demovingShift(arr, shift)
+=begin
+  input
+    arr of coded strings
+    the int shift
+  output
+    string decoded message
+=end
+end
