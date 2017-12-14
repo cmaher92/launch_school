0035c9ee3cd9f5e098c4bc06696ce22b17aae5db
Connor <connormmaher@gmail.com>
2017-12-14 18:34:49 -0500

    working on ceasar_shift

--- codewars/ceasar_cipher.rb
+++ codewars/ceasar_cipher.rb
@@ -8,33 +8,48 @@ equal length, and then the rest can go in the fifth array as long as it's
 not longer than the first
 
 init coded array
-given the string, split into an array of chars
-for each character in the array
-  unless character is a space, then just append the space
-  shift each character n number of times & append to coded
-    increment shift by 1
+init a constant for the alphabet hash, the key's will be the letter values 1-26
+  - given the string, split into an array of chars
+  - now take all of the characters and map them to their associated letter_value
+    using the ALPHABET hash, then take the value and add the shift, divmod and
+    the remainder of the divmod should become the value
+  - now that we have an array of values, map each value to the ALPHABET to get
+    the now coded character
 
-now with an array of coded characters
-join the array back
 
 divide the string into five arrays
 =end
+
+ALPHABET = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7,
+  h: 8,
+  i: 9,
+  j: 10,
+  k: 11,
+  l: 12,
+  m: 13,
+  n: 14,
+  o: 15,
+  p: 16,
+  q: 17,
+  r: 18,
+  s: 19,
+  t: 20,
+  u: 21,
+  v: 22,
+  w: 23,
+  x: 24,
+  y: 25,
+  z: 26
+}
+
 def codeString(s, shift)
-  coded = []
-  # shift
-  s.split(//).each do |char|
-    if char == ' '
-      coded << char
-      shift += 1
-      next
-    end
-    shift.times do
-      char.next
-    end
-    coded << char
-    shift += 1
-  end
-  p coded
 end
 
 def movingShift(s, shift)
