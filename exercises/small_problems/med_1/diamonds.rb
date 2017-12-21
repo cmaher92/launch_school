9b23b4140f5981761a915a3e109dc5806bbddc68
Connor Maher <connormmaher@gmail.com>
2017-12-21 23:18:29 +0000

    finished my solution for diamond problem

--- exercises/small_problems/med_1/diamonds.rb
+++ exercises/small_problems/med_1/diamonds.rb
@@ -16,47 +16,61 @@
 # spaces = 2, stars = 7
 # spaces = 0, stars = 9
 
-# 4, 1, 4
-# 3, 3, 3
-# 2, 5, 2
-# 1, 7, 1
-# 0, 9, 0
+# build a spaces arr
+#   build up to n
+#   build down to 0
+
+def build_spaces(n)
+  spaces = []
+  space_count = n-1
+  until space_count < 0
+    spaces << space_count
+    space_count -= 2
+  end
+  space_count = 0
+  until space_count >= n - 1
+    space_count += 2
+    spaces << space_count
+  end
+  spaces.map! do |space_count|
+    ' ' * (space_count/2)
+  end
+  spaces
+end
+
+def build_stars(n)
+  # build up to n
+  # build down to 0
+  star_count = 1
+  stars = []
+  until star_count == n
+    stars << star_count
+    star_count += 2
+  end
+  until star_count < 1
+    stars << star_count
+    star_count -= 2
+  end
+  stars.map! do |star_count|
+    "*" * star_count
+  end
+end
 
 def diamond(n)
-  # init diamond arr, each element will be a string
-  # stars = 1
-  # spaces = n - 1
-  # map each item of the diamond_arr
-  #   str = create_str(stars, spaces)
-  #   stars + 2
-  #   spaces - 2
-  #   str
-
-  # given 9
-  # stars = 1, spaces = 8
-  # str = '    *    '
-  # stars = 3
-  # spaces = 6
-  # str = '   ***   '
-  # stars = 5
-  # spaces = 4
-  # str = '  *****  '
+  stars  = build_stars(n)
+  spaces = build_spaces(n)
+  
+  spaces.each_with_index do |spaces_str, idx|
+    puts "#{spaces_str}#{stars[idx]}#{spaces_str}"
+  end
 end
 
-def create_str(stars, spaces)
-  half_spaces = (' ' * (spaces / 2))
-  stars = ('*' * stars)
-  p half_spaces + stars + half_spaces
-end
+# How can I refactor this?
+# when building the stars, spaces arr
+#   build strings right away, spaces should be half the size
 
 
-# [' ', ' ', ' ', ' ', '*', ' ', ' ', ' ', ' ']
-# [' ', ' ', ' ', '*', '*', '*', ' ', ' ', ' ']
-
-
-# def diamond(n)
-#   # build the first
-# end
+diamond(9)
 
 
 # Examples
