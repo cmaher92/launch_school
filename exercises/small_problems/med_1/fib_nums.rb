14ce4040f85ac4ae984bea0493d4962ab6ceab06
cmaher92 <connormmaher@gmail.com>
2017-12-24 22:04:22 -0500

    finish fib_nums problem

--- exercises/small_problems/med_1/fib_nums.rb
+++ exercises/small_problems/med_1/fib_nums.rb
@@ -13,12 +13,13 @@
 # A recursive method is one in which the method calls itself.
 #
 # For example:
-def sum(n)
-  return 1 if n == 1
-  n + sum(n - 1)
-end
-p sum(9)
+# def sum(n)
+#   return 1 if n == 1
+#   n + sum(n - 1)
+# end
+# p sum(9)
 # sum is a recursive method that computes the sum of all integers between 1 and n.
+
 #
 # Recursive methods have three primary qualities:
 # They call themselves at least once.
@@ -32,36 +33,47 @@ p sum(9)
 # Write a recursive method that computes the nth Fibonacci number,
 # where nth is an argument to the method.
 
-input
-  integer - the fibonacci sequence to compute to
-output
-  integer - the fibonacci value at the inputted position
+# input
+#   integer - the fibonacci sequence to compute to
+# output
+#   integer - the fibonacci value at the inputted position
+#
+# Determine how to do the fib sequence without using a method
+# Then translate it into a recursive method
 
-Determine how to do the fib sequence without using a method
-Then translate it into a recursive method
+# n = 9
+# prev_a = 0
+# prev_b = 1
+# prev_a + prev_b = fib
+# prev_a = prev_b
+# prev_b = fib
+# count += 1
 
-n = 9
-prev_a = 1
-prev_b = 1
-prev_a + prev_b = fib
-prev_a = prev_b
-prev_b = fib
-count += 1
+# prev_a = 1
+# prev_b = 2
+# prev_a + prev_b = fib
+# prev_a = prev_b
+# prev_b = fib
+# count += 1
 
-prev_a = 1
-prev_b = 2
-prev_a + prev_b = fib
-prev_a = prev_b
-prev_b = fib
-count += 1
+# If I pass 1, need 1 returned
+# If I pass 2, need 1 returned
+# If I pass 3, need 2 returned
+# If I pass 4, need 3 returned
 
+def fibonacci(n)
+  # because the first 2 values in fib, are 1's
+  return 1 if n == 1 || n == 2
+
+  fibonacci(n-1) + fibonacci(n-2)
+end
 
 
 # Examples:
-# fibonacci(1) == 1
-# fibonacci(2) == 1
-# fibonacci(3) == 2
-# fibonacci(4) == 3
-# fibonacci(5) == 5
-# fibonacci(12) == 144
-# fibonacci(20) == 6765
+p fibonacci(1) == 1
+p fibonacci(2) == 1
+p fibonacci(3) == 2
+p fibonacci(4) == 3
+p fibonacci(5) == 5
+p fibonacci(12) == 144
+p fibonacci(20) == 6765
