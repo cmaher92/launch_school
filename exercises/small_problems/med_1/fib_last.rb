6f5df0d4d78d81c2ecd6812396f02d5bab5cfba4
cmaher92 <connormmaher@gmail.com>
2017-12-26 02:38:52 -0500

    finish fib_last.rb problem

--- exercises/small_problems/med_1/fib_last.rb
+++ exercises/small_problems/med_1/fib_last.rb
@@ -5,11 +5,9 @@ def fibonacci_last(n)
 end
 
 def fibonacci(n)
-  # if n > 1000
-  #   n = n.digits[0..1] + [n.digits[-1]]
-  #   n = n.join.to_i
-  # end
-  # binding.pry
+  if n.digits.size > 3
+    n = n.digits.first(3).reverse.join.to_i
+  end
   last_two = [1, 1]
   3.upto(n) do |i|
     last_two = [last_two.last, (last_two.first + last_two.last)]
@@ -18,25 +16,21 @@ def fibonacci(n)
   last_two.last
 end
 
-# This solution simply mods the result by 10, drastically cutting down computing
-# time needed to solve the problem. The issue is that eventually this too
-# becomes slow. How can we modify this function to handle numbers regardless of
-# size?
+# So, the pattern repeats every 60.
+# if the num.digits.count > 3, the the last 3 and save as num
 
-# instead of statically modding the result by 10, what if we dynamically
-# modified based on the size of the sum?
-# once the result is at least
-# no change in computing speed even when increasing the size of the number.
+# After reviewing the other student's solutions the pattern approach
+# was indeed the proper approach, however the better way to handle it
+# is to mod the num by 60.
+# This will always return a number less than 60 (mod returns remainder) of
+# num / 60. So this solution calculates much faster and is easier to
+# understand. 
 
-# So, the pattern repeats every 100.
-
-# p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
-# p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
-# p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
-# # p fibonacci_last(101)
-p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
-p fibonacci_last(1001)
-# p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
-# p fibonacci_last(123456789) # -> 4
-# p fibonacci_last(123456789987745) # -> 4
-# p fibonacci_last(45)
+p fibonacci_last(15)              # -> 0  (the 15th Fibonacci number is 610)
+p fibonacci_last(20)              # -> 5 (the 20th Fibonacci number is 6765)
+p fibonacci_last(100)             # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+p fibonacci_last(100_001)         # -> 1 (this is a 20899 digit number)
+p fibonacci_last(1_000_007)       # -> 3 (this is a 208989 digit number)
+p fibonacci_last(123456789)       # -> 4
+p fibonacci_last(123456789987745) # -> 5
+# p fibonacci_last(25)
