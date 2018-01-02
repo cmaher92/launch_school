cfef50681c2645d43240a2b81e4d17bb4f30903b
cmaher92 <connormmaher@gmail.com>
2018-01-01 23:16:05 -0500

    add unlucky_days problem

---
+++ exercises/small_problems/med_2/unlucky_days.rb
@@ -0,0 +1,20 @@
+# Unlucky Days
+# Write a method that returns the number of
+# Friday the 13ths in the year given by an argument.
+# You may assume that the year is greater than 1752
+# (when the United Kingdom adopted the modern Gregorian Calendar)
+# and that it will remain in use for the foreseeable future.
+
+# input
+#   integer - representing a year
+# output
+#   integer - number of Friday the 13ths in a given year
+# rules
+#   you can assume the year is after 1752
+
+# 
+
+# Examples:
+friday_13th(2015) == 3
+friday_13th(1986) == 1
+friday_13th(2019) == 2
