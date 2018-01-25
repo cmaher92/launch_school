ff40e167ac783ee30b6b7f53f476fdb8dded5d8c
cmaher92 <connormmaher@gmail.com>
2018-01-24 20:52:08 -0500

    finish prize_draw.rb problem

--- exercises/codewars/prize_draw.rb
+++ exercises/codewars/prize_draw.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 LETTER_VALUE = {
   'a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5, 'f' => 6,
   'g' => 7, 'h' => 8, 'i' => 9, 'j' => 10, 'k' => 11, 'l' => 12,
@@ -9,29 +7,9 @@ LETTER_VALUE = {
 }
 
 def rank(st, we, n)
-    # input
-    # st - string of firstnames
-    # we - an array of weights
-    # n  - 4, the winning name is in the 4th position
-    # output
-    # string - winning name
-    # rules
-    #  if st is empty return "No participants"
-    #  if n is greater than the number of participants return "Not enough participants"
-
-    # given a string of firstnames, split the names on the comma
-    # map each name in the array of names returned from the split function
-    #   name = 'COLIN'
-    #   split the name into chars
-    #   name = ['C', 'O', 'L', 'I', 'N']
-    #   reduce the char array passing sum, and char
-    #     sum = 0, char = 'C'
-    #     first convert the letter to a number using the LETTER_VALUE constant
-    #     char = 3
-    #     then return sum + char_value
-    #     0 + 3
-    #   return the value returned from the reduce method to the map method
+    return 'No participants' if st.empty?
     st = st.split(',')
+    return 'Not enough participants' if n > st.length
     values = st.map.with_index do |name, idx|
       name_chars = name.chars
       value = name_chars.reduce(0) do |sum, char|
@@ -41,13 +19,26 @@ def rank(st, we, n)
       {name => (value + name.length) * we[idx]}
     end
 
-    # sort the array by the value in each hash
-    values = values.map { |hash| binding.pry hash.to_a }
-    binding.pry
+    sorted_values = values.sort_by { |name| name.values }
+    sorted_values.map! { |name| name.to_a }
+    sorted_values[n - 1][0][0]
 end
 
+# Test cases
 st = "COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH"
 we = [1, 4, 4, 5, 2, 1]
 n  = 4
 
-rank(st, we, n)
+p rank(st, we, n)
+
+st = ""
+we = [1, 2, 3, 4, 5]
+n  = 3
+
+p rank(st, we, n)
+
+st = "COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH"
+we = [1, 4, 4, 5, 2, 1]
+n  = 9
+
+p rank(st, we, n)
