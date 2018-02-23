1cebfa03e28e9d16021a1fd42d07600143940cc7
cmaher92 <connormmaher@gmail.com>
2018-02-23 18:04:11 -0500

    broke down instructor solution

--- exercises/small_problems/hard_1/seeing_stars.rb
+++ exercises/small_problems/hard_1/seeing_stars.rb
@@ -37,6 +37,13 @@
 
 require 'pry'
 
+# outer rows function
+#
+# input
+#   row, n
+# output
+#
+
 # def top_rows(spaces, n)
 #   return if spaces == 0
 #   row = "***"
@@ -59,19 +66,58 @@ require 'pry'
 #   bottom_rows("***", n)
 # end
 
-def print_row(grid_size, distance_from_center)
-  number_of_spaces = distance_from_center - 1
+# instructor solution
+# star method
+#   sets the max_distance variable to half of n, - 1
+#   from max_distance down to 1
+#     each block will be passed the distance, and n
+#     calls print_row func
+#   prints the middle of the star by multiplying the star by n
+#   from 1 up to max_distance
+#     each block will be passed the distance, and n
+#     calls print_row func
+
+# print_row
+# input
+#    n - the grid size
+#    distance - distance from the center of the star
+# sets the number_of_spaces to the distance - 1
+# sets spaces to a space multiplied by number_of_spaces
+# sets output to a new array containing 3 *'s
+# joins the output with spaces previously defined, centers and then prints
+
+require 'pry'
+
+def print_row(n, distance)
+  number_of_spaces = distance - 1
   spaces = ' ' * number_of_spaces
-  output = Array.new(3, '*').join(spaces)
-  puts output.center(grid_size)
+  output = Array.new(3, '*')
+  puts output.join(spaces).center(n)
 end
 
-def star(grid_size)
-  max_distance = (grid_size - 1) / 2
-  max_distance.downto(1) { |distance| print_row(grid_size, distance) }
-  puts '*' * grid_size
-  1.upto(max_distance)   { |distance| print_row(grid_size, distance) }
+def star(n)
+  max_distance = (n - 1) / 2
+  max_distance.downto(1) do |distance|
+    print_row(n, distance)
+  end
+  puts '*' * n
+  1.upto(max_distance) do |distance|
+    print_row(n, distance)
+  end
 end
+
+# notes
+# I liked that the instructor utilized an array coupled with the join method
+# that was a clever way to easily add spaces inbetween the stars opposed
+# to using insert
+# Also using the downto method was easily a better and more concise solution
+# as opposed to using a recursive solution which ended up being too much
+# for the problem as it seemed to require two seperate ones depending on
+# whether or not it was the top of the star or the bottom of the star
+# I'm still curious if I could abstract a simple recursive method that could
+# recognize whether or not it was the top row or bottom row, but again
+# it seems like it's simply overkill at this point. 
+
 
 # Examples
 star(7)
@@ -95,6 +141,3 @@ star(9)
 #   * * *
 #  *  *  *
 # *   *   *
-
-# TODO
-# now abstract top_rows and bottom_rows into a single recursive function
