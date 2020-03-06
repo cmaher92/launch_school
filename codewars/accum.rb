68d689ba71daed86b406ace188078aceb8867e0a
connormmaher <connormmaher@gmail.com>
2020-03-06 08:31:46 -0500

    consolidate codewars exercises

--- codewars/accum.rb
+++
@@ -1,5 +0,0 @@
-def accum(s)
-  s.chars.each_with_index.map{ |c, i| c.upcase + c.downcase * i }.join('-')
-end
-
-puts accum("ZpglnRxqenU")
--- codewars/triboacci.rb
+++
@@ -1,8 +0,0 @@
-def tribonacci(s, n)
-  for i in 3..n
-    s[i] = s[i-1] + s[i-2] + s[i-3]
-  end
-  return s.slice(0, n)
-end
-
-puts tribonacci([1, 1, 1], 10)
