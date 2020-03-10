dbcc8e3b1fc2d138e6c065b6dda71087c9b2277e
cmaher92 <connormmaher@gmail.com>
2020-03-10 14:11:58 -0400

    finish consonant value problem, kyu 6

---
+++ exercises/codewars/consonant_value.rb
@@ -0,0 +1,27 @@
+# consonant value
+# https://www.codewars.com/kata/59c633e7dcc4053512000073
+
+require 'pry'
+
+# input
+#   string; lowercase, alphabetic chars only, no spaces
+# output
+#   int; the highest value substring
+# rules
+#   consonants are any letters of the alphabet except 'aeiou'
+
+# split the given str on any vowels
+
+def solve(str)
+  str.split(/[aeiou]/).map! do |substring|
+    substring.bytes.map { |value| value - 96 }.reduce(&:+)
+  end.reject { |value| value == nil }.sort[-1]
+end
+
+puts solve("zodiac") == 26
+puts solve("chruschtschov") == 80
+puts solve("khrushchev") == 38
+puts solve("strength") == 57
+puts solve("catchphrase") == 73
+puts solve("twelfthstreet") == 103
+puts solve("mischtschenkoana") == 80
