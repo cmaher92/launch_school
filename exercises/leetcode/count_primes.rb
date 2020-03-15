5412f91599360d3732a3f809ad2e288d21510489
Connor Maher <connormmaher@gmail.com>
2020-03-14 21:34:55 -0400

    work on practice problems

--- exercises/leetcode/count_primes.rb
+++ exercises/leetcode/count_primes.rb
@@ -1,27 +1,21 @@
 require 'pry'
 
-def is_prime?(num)
-  # return true if prime
-  # return false if not prime
-  (2..num).each do |n|
-    next if n == num
-    return false if num % n == 0
-  end
-  true
-end
+# def is_prime?(num)
+# end
+#
+# p is_prime?(10)
+# p is_prime?(2)
 
 def count_primes(n)
-  # input: int
-  # output: int
-  # count the number of prime nums less than a non-negative input
-  # A prime number is a numeral that is greater than 1
-  # and cannot be divided evenly by any other number except 1 and itself
-  results = (1..n).select do |num|
-    # check each num to see if it's prime
-    # if it's prime keep it
-    is_prime?(num)
-  end
+  # The Sieve of Eratosthenes
+  # create an array of n numbers
+  # start with the first number (2)
+    # remove all multiples of that number
 
-  results.count
+  table = Array.new(n) { |i| i }
+  2.times { |i| table.delete(i) }
+  
 
 end
+
+# p count_primes(10)
