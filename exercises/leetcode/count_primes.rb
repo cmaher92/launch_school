f59ddac27fca783d5c7dc2bce9a7ee2d493334ac
Connor Maher <connormmaher@gmail.com>
2020-03-15 22:12:50 -0400

    finish first solution of count primes

--- exercises/leetcode/count_primes.rb
+++ exercises/leetcode/count_primes.rb
@@ -6,16 +6,32 @@ require 'pry'
 # p is_prime?(10)
 # p is_prime?(2)
 
-def count_primes(n)
-  # The Sieve of Eratosthenes
-  # create an array of n numbers
-  # start with the first number (2)
-    # remove all multiples of that number
+# given a number (n)
+# count the number of prime numbers from 1 - n
+# start at 2, 1 is not a prime number
+# n / 2, 3, 4, 5, 6, etc..
 
-  table = Array.new(n) { |i| i }
-  2.times { |i| table.delete(i) }
-  
-
+def count_primes(number)
+  # create an array from 2-num
+  # for each number in the array of possible primes
+    # modulo the number by each n
+    # if the number % n is 0, it's not a prime number
+    # unless the number and n are the same
+  possible_primes = (2...number).to_a
+  possible_primes.select! do |num|
+    # need to return true if num is prime, false if it is not
+    # binding.pry if num == 3
+    results = (2...num).select do |n|
+      # if num % n == 0, means that n evenly divides into num making it not prime
+      num % n == 0
+    end
+    results.empty?
+    # any number within results evenly divided into the num
+    # making the num not prime
+    # so if results is empty, it means that nothing divided into the num
+    # making the num prime
+  end
+  possible_primes.count
 end
 
-# p count_primes(10)
+p count_primes(10) == 4
