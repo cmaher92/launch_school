d545f004dfb52cc021ca13da32397882dcfa21bc
Connor Maher <connormmaher@gmail.com>
2020-03-18 00:42:31 -0400

    work on str_mix problem, not done

--- exercises/codewars/str_mix.rb
+++ exercises/codewars/str_mix.rb
@@ -27,21 +27,33 @@ require 'pry'
 # ['a', 'b']
 
 def mix(s1, s2)
-    s1 = s1.scan(/[a-z]{2,}/)
-    s2 = s2.scan(/[a-z]{2,}/)
-    s1.map! { |letters| [letters[0],letters.length] }
-    s2.map! { |letters| [letters[0],letters.length] }
-    s1 = s1.to_h
-    s2 = s2.to_h
-    letters = s1.keys + s2.keys
+    s1 = s1.scan(/[a-z]/)
+    s2 = s2.scan(/[a-z]/)
+
+    # sort
+    # turn array of chars into a hash, with a count of each char
+    # select only key,value where value > 2
+    h1 = {}
+    h2 = {}
+    s1.each do |char|
+        h1[char] == nil ? h1[char] = 1 : h1[char] += 1
+    end
+    s2.each do |char|
+        h2[char] == nil ? h2[char] = 1 : h2[char] += 1
+    end
+    h1 = h1.sort_by { |k, v| v }.reverse.to_h
+    h2 = h2.sort_by { |k, v| v }.reverse.to_h
+    [h1, h2].each { |hsh| hsh.select! { |k, v| v >= 2 }}
+
+    letters = h1.keys + h2.keys
     letters.uniq!
 
     # map letters
         # for each letter
         # see if s1, s2 or equivalent
     letters.map! do |letter|
-        l1 = s1.fetch(letter, 0)
-        l2 = s2.fetch(letter, 0)
+        l1 = h1.fetch(letter, 0)
+        l2 = h2.fetch(letter, 0)
         result = l1 <=> l2
         case result
         when 0
@@ -52,14 +64,25 @@ def mix(s1, s2)
             "2:#{letter * l2}"
         end
     end
-    letters.join('/')
+    answer = []
+    letters.select! do |group| 
+        if group[0] == "="
+            answer << group
+            false
+        else
+            true
+        end
+    end
+    letters
+            
 end
 
 
-s1 = "A aaaa bb c"
-s2 = "& aaa bbb c d"
-p mix(s1, s2) == "1:aaaa/2:bbb"
+# s1 = "A aaaa bb c"
+# s2 = "& aaa bbb c d"
+# p mix(s1, s2) == "1:aaaa/2:bbb"
 p mix("Are they here", "yes, they are here")# == "2:eeeee/2:yy/=:hh/=:rr"
 
 # to do
-# the case statement is messing with the output, i think it's how I'm creating the string
+# sort the letters by the length of the string using str[2, str.length - 2]
+# refactor this, i think it could be shorter
