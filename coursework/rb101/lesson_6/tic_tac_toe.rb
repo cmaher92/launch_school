09b4d8b75070fe6226a06d70b1411d0dc1e2de34
Connor Maher <connormmaher@gmail.com>
2020-04-18 00:45:07 -0400

    finish TTT walkthrough

--- coursework/rb101/lesson_6/tic_tac_toe.rb
+++ coursework/rb101/lesson_6/tic_tac_toe.rb
@@ -11,13 +11,20 @@
 # 9. If yes, go to #1
 # 10. Good bye!
 
+require 'pry'
+
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = ' ' }
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def display_board(board)
+  system 'clear'
   puts ""
   puts "     |     |"
   puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}"
@@ -37,14 +44,70 @@ def prompt(msg)
   puts ">> #{msg}"
 end
 
-def player_places_piece(board)
-  prompt "Choose a square (1-9):"
-  square = gets.chomp.to_i
-  board[square] = 'X'
+def empty_squares(board)
+  board.keys.select { |num| board[num] == INITIAL_MARKER }
 end
 
-board = initialize_board
-display_board(board)
+def player_places_piece!(board)
+  square = ''
 
-player_places_piece(board)
-display_board(board)
+  loop do 
+    prompt "Choose a square (#{empty_squares(board).join(', ')}):"
+    square = gets.chomp.to_i
+    break if empty_squares(board).include?(square)
+    prompt("Sorry that's not a valid choice")
+  end
+
+  board[square] = PLAYER_MARKER
+end
+
+def computer_places_piece!(board)
+  square = empty_squares(board).sample
+  board[square] = COMPUTER_MARKER
+end
+
+def board_full?(board)
+  empty_squares(board).empty?
+end
+
+def someone_won?(board)
+  !!detect_winner(board)
+end
+
+def detect_winner(board)
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                  [[1, 5, 9], [3, 5, 7]]              # diagnals
+  winning_lines.each do |line|
+    return 'Player' if line.all? { |pos| board[pos] == PLAYER_MARKER }
+    return 'Computer' if line.all? { |pos| board[pos] == COMPUTER_MARKER }
+  end
+  nil
+end
+
+# game loop
+loop do
+  board = initialize_board
+
+  loop do
+    display_board(board)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+    
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "Play again (y or n)"
+  answer = gets.chomp
+  break if answer == 'n'
+end
+
+prompt "Thanks for playing tic tac toe. Goodbye!"
