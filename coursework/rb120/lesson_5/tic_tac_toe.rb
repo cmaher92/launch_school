79e58be5dc751dfb505510d56e9307bb94701d5b
Connor Maher <connormmaher@gmail.com>
2020-07-08 01:03:02 -0400

    begin namespacing TTT classes

--- coursework/rb120/lesson_5/tic_tac_toe.rb
+++ coursework/rb120/lesson_5/tic_tac_toe.rb
@@ -1,5 +1,4 @@
 require 'paint'
-require 'pry'
 
 module Displayable
   EMPTY_SPACE = ""
---
+++ coursework/rb120/lesson_5/tic_tac_toe/board.rb
@@ -0,0 +1,54 @@
+module Tictactoe
+  class Board
+    UNMARKED_POSITIONS = [
+      [0, 0], [0, 1], [0, 2],
+      [1, 0], [1, 1], [1, 2],
+      [2, 0], [2, 1], [2, 2]
+    ]
+
+    def initialize
+      @number_of_squares = 9
+      @number_of_marked = 0
+      @number_of_unmarked = 9
+      @unmarked_square_positions = UNMARKED_POSITIONS
+      @board = build_board
+    end
+
+    def place_piece
+      # TODO
+    end
+
+    def draw
+      # TODO
+    end
+
+    def blank?
+      # TODO
+    end
+
+    def contents_of(position)
+      # TODO
+    end
+
+    private
+
+    def build_board
+      board = Array.new(3) { Array.new(3) { Square.new } }
+    end
+
+  end
+
+  class Square
+    attr_accessor :marker
+
+    def to_s
+      if marker == 'X'
+        Paint['X', '#00cc66', :bold]
+      elsif marker == 'O'
+        Paint['O', '#bf0603', :bold]
+      else
+        " "
+      end
+    end
+  end
+end
