4dd0c0da5673ecda147a1a846f696da3999cd3c9
Connor Maher <connormmaher@gmail.com>
2020-07-19 00:20:45 -0400

    fix rubocop offenses

--- coursework/rb120/lesson_5/21/hand.rb
+++ coursework/rb120/lesson_5/21/hand.rb
@@ -3,7 +3,8 @@ require 'pry'
 
 module TwentyOne
   class Hand
-    HIT = [' ' * 5, ' ' * 5, ' ' * 5, 'HIT'.center(5), ' ' * 5, ' ' * 5, ' ' * 5]
+    EMPTY = '     '
+    HIT = [EMPTY, EMPTY, EMPTY, 'HIT'.center(5), EMPTY, EMPTY, EMPTY]
     attr_reader :hand, :value
 
     def initialize
@@ -15,13 +16,13 @@ module TwentyOne
     def busted?
       @busted
     end
-    
+
     def blackjack?
       @hand.first(2).map(&:value).sum == 21
     end
-    
+
     def reveal
-      @hand.each { |card| card.reveal }
+      @hand.each(&:reveal)
     end
 
     def <<(card)
@@ -40,7 +41,7 @@ module TwentyOne
     end
 
     def to_s
-      displayable_move_and_hand.join("\n")
+      hand_details.join("\n")
     end
 
     private
@@ -50,40 +51,44 @@ module TwentyOne
     end
 
     def calc_value_for_ace
-      if @value > 21
-        @hand.select(&:ace?).first.value = 1 # set aces value to 1
-        @value -= 10
-      end
+      return if @value < 22
+      @hand.select(&:ace?).first.value = 1 # set aces value to 1
+      @value -= 10
     end
 
     def displayable_value
-      value = @hand.reject { |card| card.hidden? }.map(&:value).sum
-      [' ', ' ', ' ', "#{"TOTAL:".center(8)}", "#{value.to_s.center(8)}", ' ', ' ']
+      value = @hand.reject(&:hidden?).map(&:value).sum
+      [' ', ' ', ' ', "TOTAL:".center(8), value.to_s.center(8), ' ', ' ']
     end
 
-    def displayable_move_and_hand
-      cards = @hand.map(&:displayable)
+    def hand_details
+      # takes cards, hits, and hand value and combines to display
+      # on a single row
+      # [] [] HIT [] TOTAL
+      cards_hits_and_total = retrieve_cards_hits_and_total
 
-      # add HITs
+      hand_details = []
+      7.times do |row_idx|
+        row = []
+        cards_hits_and_total.each { |card| row << card[row_idx] }
+        hand_details << row.join('')
+      end
+      hand_details
+    end
+
+    def retrieve_cards_hits_and_total
+      # adds text to indicate when participant hits and value of hand
+      cards = @hand.map(&:displayable)
+      add_hits(cards) << displayable_value
+    end
+
+    def add_hits(cards)
       cards_and_hits = []
       cards.each_with_index do |card, idx|
         cards_and_hits << HIT if idx > 1
         cards_and_hits << card
       end
-
-      # add score
-      cards_and_hits << displayable_value
-
-      # combine cards, hits, score so they can display next to each other
-      displayable_hand = []
-      7.times do |row_idx|
-        row = []
-        cards_and_hits.each { |card| row << card[row_idx] }
-        displayable_hand << row.join('')
-      end
-      # binding.pry
-      displayable_hand
+      cards_and_hits
     end
-
   end
 end
