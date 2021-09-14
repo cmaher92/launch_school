93603834ab0e7c7cd5736c64d2a00beef9ef9eaa
Connor Maher <connormmaher@gmail.com>
2021-09-13 20:22:45 -0400

    working in koans as a warm up

--- exercises/leetcode/add_digits.rb
+++ exercises/leetcode/add_digits.rb
@@ -3,17 +3,17 @@ def add_digits(num)
   # integer
   # output
   # single digit number
-  
+
   # return if number is single digit
-    # divide the number by 10, if it's less than one return the number
+  # divide the number by 10, if it's less than one return the number
   # if number is not single digit
-    # split the number into digits using integer#digits method
-    # sum the numbers using Array#reduce
-    # call add_digits passing the current number
-    
+  # split the number into digits using integer#digits method
+  # sum the numbers using Array#reduce
+  # call add_digits passing the current number
+
   return num if (num / 10) < 1
+
   add_digits(num.digits.reduce(:+))
-  
 end
 
 # test cases
