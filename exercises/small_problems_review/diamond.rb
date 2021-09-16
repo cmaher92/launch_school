e635e6657670df7640fd45cca6b78a0ddb562e3f
connormmaher <connormmaher@gmail.com>
2021-09-16 10:35:49 -0400

    finish diamond problems with accompanying tests

--- exercises/small_problems_review/diamond.rb
+++ exercises/small_problems_review/diamond.rb
@@ -1,40 +1,58 @@
 require 'pry'
-# require 'minitest/autorun'
+require 'minitest/autorun'
 #
 
 # Details:
-# Input:
-# Output:
+# Input: integer: 'n' (will always be odd)
+# Output: displays a diamond
 # Requirements:
+# - 
 
 # EXAMPLES
-# var: 9 --> 11
+
 
 # DATA STRUCTURE
 
 # ALGORITHM
-# 1.
+# 1. create an array from 1...n then from n..-1 excluding even values
+#   9 --> [1, 3, 5, 7], [9, 7, 5, 3, 1]
+# 2. combine both newly created arrays
+# 3. replace the integer in the new array with '*''s
 
-def diamond(num, output: $stdout)
-  output.puts " *\n***\n *"
+def diamond(num)
+    stars = (1...num).step(2).to_a + (num..1).step(-2).to_a
 
+    stars.map! do |num_of_stars|
+        ('*' * num_of_stars).center(num).rstrip
+    end
+    
+    puts stars
 end
 
-io = StringIO.new
-diamond(1,output: io)
-binding.pry
+diamond(9)
 
-# class Test < Minitest::Test
-#   def setup
-#     String
-#   end
+class Test < Minitest::Test
+  def setup
+  end
 
-#   def test_diamond
-#     # test
-#     # assert_output(@one) { diamond(1) }
-#     # assert_output(@three) { diamond(3) }
-#     io = StringIO.new
-#     binding.pry
-#     assert diamond(3, output: io)
-#   end
-# end
+  def test_diamond_single_output
+    expect = "*\n"
+    assert_output(expect) { diamond(1) }
+  end
+
+  def test_diamond
+    expect = <<STAR
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+STAR
+    assert_output(expect) { diamond(9) }
+  end
+
+end
